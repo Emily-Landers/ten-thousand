cedfe09f183613df2de254ce57ad07d70b5d76df
Emily Landers <emily.j.landers@gmail.com>
2022-04-25 14:47:46 -0400

    more setup

---
+++ ten_thousand/game_logic.py
@@ -0,0 +1,26 @@
+class GameLogic():
+    
+    @staticmethod 
+    def calculate_score():
+        pass
+#The input to calculate_score is a tuple of integers that represent a dice roll.
+#The output from calculate_score is an integer representing the roll’s score according to rules of game.
+    
+    def roll_dice():
+        pass
+#The input to roll_dice is an integer between 1 and 6.
+#The output of roll_dice is a tuple with random values between 1 and 6.
+#The length of tuple must match the argument given to roll_dice method.
+    
+class Banker(self):
+    def shelf(self):
+        pass
+#Input to shelf is the amount of points (integer) to add to shelf.
+#shelf should temporarily store unbanked points.
+    def bank():
+        pass
+#bank should add any points on the shelf to total and reset shelf to 0.
+#bank output should be the amount of points added to total from shelf.
+    def clear_shelf():
+        pass
+#clear_shelf should remove all unbanked points.
